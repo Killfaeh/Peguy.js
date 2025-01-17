1d7000991b87ec7a9a1bf1a747258038d8bf7c60
Suisei <44418071+Killfaeh@users.noreply.github.com>
2025-01-17 16:31:18 +0100

    Update

--- PeguyJS/Utils/Peguy.js
+++ PeguyJS/Utils/Peguy.js
@@ -5,7 +5,27 @@ var PEGUY =
 	mode: 'classic',
 	language: navigator.language || navigator.userLanguage,
 	userAgent: navigator.userAgent,
-	platform: navigator.platform
+	platform: navigator.platform,
+
+	appendToMain: function($node)
+	{
+		document.getElementById('main').appendChild($node);
+	},
+
+	appendToScreen: function($node)
+	{
+		document.getElementById('screen').appendChild($node);
+	},
+
+	emptyScreen: function()
+	{
+		document.getElementById('screen').empty();
+	},
+
+	mousePosition: function($event)
+	{
+		return document.getElementById('main').mousePosition($event);
+	}
 };
 
 if (Loader !== null && Loader !== undefined)
